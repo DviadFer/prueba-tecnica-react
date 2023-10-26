8feefd2fd31031eb284b55823cf003a289845f51
DviadFer <diegoviadfer@gmail.com>
2023-10-26 17:57:52 +0200

    Create api connections and react router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./views/home";
+// import PodcastDetail from "./views/podcastDetail";
+// import EpisodeDetail from "./views/episodeDetail";
 
 function App() {
-
   return (
-    <>
-      <div>App</div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/podcast/:podcastId" element={<PodcastDetail />} />
+        <Route path="/podcast/:podcastId/episode/:episodeId" element={<EpisodeDetail />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+
+export const fetchTopPodcasts = () => {
+  return fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
+    .then(response => response.json())
+    .then(data => data.feed.entry);
+};
+
+export const fetchPodcastDetails = (podcastId) => {
+  return fetch('https://itunes.apple.com/lookup?id=${podcastId}')
+    .then(response => response.json())
+    .then(data => data.results[0]);  // Ajusta esto según la estructura de la respuesta
+};
+
+export const fetchPodcastEpisodes = (podcastId) => {
+  return fetch('https://itunes.apple.com/lookup?id=${podcastId}&entity=podcastEpisode&limit=9')
+    .then(response => response.json())
+    .then(data => data.results.slice(1)); // El primer resultado suele ser el podcast mismo, por lo que usamos slice para obtener solo episodios
+};
